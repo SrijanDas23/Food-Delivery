2d6f04c3772baf1b3f23ae16e7d244ace1ca6f83
SrijanDas23 <157733127+SrijanDas23@users.noreply.github.com>
2024-07-14 20:17:34 +0530

    created the models

--- src/main/java/com/srijan/model/User.java
+++ src/main/java/com/srijan/model/User.java
@@ -1,4 +1,41 @@
 package com.srijan.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.srijan.dto.RestaurantDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private USER_ROLE role;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "customer")
+    private List<Order> orders=new ArrayList<>();
+
+    @ElementCollection
+    private List<RestaurantDto> favourites=new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses=new ArrayList<>();
 }
